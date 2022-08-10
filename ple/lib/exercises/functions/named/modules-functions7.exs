05a392861c98c6b6af1b7505bac7759ee84e9c26
Julio Estrada <me@julioestrada.dev>
2022-08-09 23:43:02 -0600

    feat: erlang use in elixir and used some standard library packages
    Signed-off-by: Julio Estrada <me@julioestrada.dev>

---
+++ ple/lib/exercises/functions/named/modules-functions7.exs
@@ -0,0 +1,9 @@
+IO.puts(:io_lib.format("~.2f", [2.88999]))
+
+IO.puts(System.get_env("HOME"))
+
+IO.puts(Path.extname("./ple/lib/exercises/functions/named/modules-functions7.exs"))
+
+IO.puts(System.pid())
+
+System.cmd("ls", ["-l"])
